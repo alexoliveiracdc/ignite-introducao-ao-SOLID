77dd7e143fe52b1a4c0375da5e1f6871d017174e
unknown <you@example.com>
2022-06-21 23:25:26 -0300

    desafio finalizado

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,18 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      return (
+        response
+          .status(200)
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          .json(this.turnUserAdminUseCase.execute({ user_id }))
+      );
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
